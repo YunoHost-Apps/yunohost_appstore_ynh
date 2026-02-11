7ac752afb6956e8e7aeee9de22d38b4921fa40ae
Félix Piédallu <felix@piedallu.me>
2026-02-11 17:08:28 +0100

    update_catalog_notify.sh: Also send an email on failure

--- conf/update_catalog_notify.sh
+++ conf/update_catalog_notify.sh
@@ -11,3 +11,8 @@ else
 fi
 
 curl --get --data-urlencode "status=$status" --data-urlencode "msg=$msg" "https://status.yunohost.org/api/push/$ID"
+
+if [ "$EXIT_STATUS" != "0" ]; then
+    tail -n 30 /var/log/__APP__/update_catalog.log \
+        | mail -s "Error updating __APP__ !" root
+fi
