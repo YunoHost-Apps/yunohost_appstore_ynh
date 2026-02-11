f25b30674b4f94ef850e4227b34a47f0dc2b41ae
Félix Piédallu <felix@piedallu.me>
2026-02-11 19:25:04 +0100

    Update the content of the notification email

--- conf/update_catalog_notify.sh
+++ conf/update_catalog_notify.sh
@@ -13,6 +13,10 @@ fi
 curl --get --data-urlencode "status=$status" --data-urlencode "msg=$msg" "https://status.yunohost.org/api/push/$ID"
 
 if [ "$EXIT_STATUS" != "0" ]; then
-    tail -n 30 /var/log/__APP__/update_catalog.log \
-        | mail -s "Error updating __APP__ !" root
+    mail -s "Error updating __APP__ !" root <<EOF
+Here is the end of the log:
+
+$(tail -n 30 /var/log/appstore/update_catalog.log)
+
+EOF
 fi
